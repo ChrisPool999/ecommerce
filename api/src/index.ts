a6f5fa906b4107b929f6f7ce6bc85d46456016b9
Chris Pool <brisbool999@gmail.com>
2026-02-22 05:58:29 -0800

    feat(cors): update cors to include prod url

--- api/src/index.ts
+++ api/src/index.ts
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://ecommerce-web-sze7.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
   })
